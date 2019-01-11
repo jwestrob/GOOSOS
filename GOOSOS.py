e4cd041bd9279bf04027ec96e68de150cfcf6a8a
Jacob <cobe44@gmail.com>
2019-01-10 21:40:17 -0800

    generic nonsense

--- GOOSOS.py
+++ GOOSOS.py
@@ -352,7 +352,7 @@ def prot_workflow():
 def parse_hmmdomtbl(outdir, hmmoutfile):
     goosos_dir = sys.argv[0].split('GOOSOS.py')[0]
     genome_id = hmmoutfile.split('_hmmsearch.out')[0].split('.fasta')[0].split('.fna')[0].split('.fa')[0]
-    os.system('bash ' + goosos_dir + '/hmmscan_parser.sh ' + hmmoutfile + ' > ' +  outdir + '/hmmscan/' +
+    os.system('bash ' + goosos_dir + 'hmmscan_parser.sh ' + hmmoutfile + ' > ' +  outdir + '/hmmscan/' +
                                                             genome_id + '/' + genome_id + '.parse')
     return
 
