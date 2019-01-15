8020a1253abb08e23092ebd92a475170dafbb4e2
Jacob <cobe44@gmail.com>
2019-01-15 10:32:30 -0800

    generic nonsense

--- GOOSOS.py
+++ GOOSOS.py
@@ -407,6 +407,8 @@ def test():
     all_df_list = list(p.map(lambda x: pd.read_csv(x, sep='\t'), hmm_outfiles))
     all_df = pd.concat(all_df_list, sort=False)
 
+    all_df.to_csv(outdir + '/all_hits_evalues_df.tsv', sep='\t', index=False)
+
     recs_list_by_hmm = extract_hits(all_df, threads, outdir)
 
     #Make directory to store fasta hits
