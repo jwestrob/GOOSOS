58c61a2109f0bb054ee0933aaeaf6e8f6eec1e94
jwestrob <cobe44@gmail.com>
2019-06-08 17:23:29 -0700

    generic nonsense

--- GOOSOS.py
+++ GOOSOS.py
@@ -420,6 +420,8 @@ def run_hmms(fastafile, outdir, threshold, best, cut_nc, cut_ga):
 
 def mark_with_threshold(all_df, hmm_thresh_dict):
     print('mark with threshold has been called')
+    print(hmm_thresh_dict)
+    sys.exit()
     if hmm_thresh_dict is not  None:
         all_df['above_threshold'] = all_df.apply(lambda x: x.overall_bitscore >= hmm_thresh_dict[x.family_hmm])
 
