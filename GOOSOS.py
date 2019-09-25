7722aec603d17f27a0cc6d9e9d5e7c53ed09a55b
jwestrob <cobe44@gmail.com>
2019-09-25 16:01:17 -0700

    didn't quite work yet

--- GOOSOS.py
+++ GOOSOS.py
@@ -106,13 +106,13 @@ def run_hmmscan(protfile, outdir, threshold, best, cut_nc, cut_ga):
     #print(protein_id, hmmfile)
     if not cut_nc and not cut_ga:
         cmd = 'hmmscan --domtblout ' + outdir + '/hmmscan/' + genome_id + '/' + genome_id + '_hmmsearch.out  --notextw --cpu ' \
-                + str(1) + ' -E ' + str(threshold) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm ' + protfile + ' > /dev/null 2>&1'
+                + str(1) + ' -E ' + str(threshold) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm $(realpath ' + protfile + ') > /dev/null 2>&1'
     elif cut_nc:
         cmd = 'hmmscan --domtblout ' + outdir + '/hmmscan/' + genome_id + '/' + genome_id + '_hmmsearch.out  --notextw --cut_nc --cpu ' \
-                + str(1) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm ' + protfile + ' > /dev/null 2>&1'
+                + str(1) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm $(realpath ' + protfile + ') > /dev/null 2>&1'
     elif cut_ga:
         cmd = 'hmmscan --domtblout ' + outdir + '/hmmscan/' + genome_id + '/' + genome_id + '_hmmsearch.out  --notextw --cut_ga --cpu ' \
-                + str(1) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm ' + protfile + ' > /dev/null 2>&1'
+                + str(1) + ' ' + outdir + '/hmmpress/concatenated_hmms.hmm $(realpath ' + protfile + ') > /dev/null 2>&1'
     #print(cmd)
     result = subprocess.run(cmd, shell=True, check=True)
     if result.returncode != 0:
