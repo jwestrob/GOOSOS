f448a2f8d0de570c9569ce3ee0134450be723be5
jwestrob <cobe44@gmail.com>
2020-07-07 01:53:50 -0700

    Refactored extract_hits to accommodate large input files (>1GB); will test efficiency on different input sizes later

--- GOOSOS.py
+++ GOOSOS.py
@@ -166,6 +166,39 @@ def extract_hits(all_df, threads, outdir):
 
     return recs_by_hmm
 
+def extract_hits_2(all_df, threads, outdir):
+    #Takes all_df and generates recs for each HMM by extracting on a
+    #file-by-file basis (bc of disk I/O limitations with large files)
+
+    recs_by_file = []
+    recs_by_hmm = []
+
+
+    def grab_recs_by_genome(genome_id, outdir=outdir):
+        #Get all ORFs corresponding to that genome/input file
+        desired_orfs = all_df[all_df.genome_id == genome_id].orf_id.tolist()
+        genome_dir = outdir + '/hmmsearch/' + genome_id + '/'
+        #Grab protein file from directory within hmmsearch folder
+        protfile = list(filter(lambda x: x.endswith('.faa') or x.endswith('.fa'), os.listdir(genome_dir)))[0]
+
+        genome_recs = list(filter(lambda x: x.id in desired_orfs, SeqIO.parse(os.path.join(genome_dir, protfile), 'fasta')))
+        return genome_recs
+
+    p2 = Pool(threads)
+    recs_by_file = list(p2.map(grab_recs_by_genome,  all_df.genome_id.unique().tolist()))
+
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
+    all_recs = flatten(recs_by_file)
+
+    for hmm in all_df.family_hmm.unique().tolist():
+        desired_orfs_2 = all_df[all_df.family_hmm == hmm].orf_id.tolist()
+        hmm_recs = list(filter(lambda x: x.id in desired_orfs_2, all_recs))
+        recs_by_hmm.append([hmm_recs, hmm])
+
+    return recs_by_hmm
+
 def make_hitstable_df(recs_by_hmm, hmmlist, fastalist, outdir):
 
     # Make matrix of zeros to store hits
@@ -635,8 +668,8 @@ def main():
         all_df = pd.read_csv(outdir + '/all_hits_evalues_df.tsv', sep='\t')
 
 
-    recs_list_by_hmm = extract_hits(all_df, threads, outdir)
-
+    #recs_list_by_hmm = extract_hits(all_df, threads, outdir)
+    recs_list_by_hmm = extract_hits_2(all_df, threads, outdir)
     #Make directory to store fasta hits
     if not os.path.exists(outdir + '/' + 'fastas'):
         os.system('mkdir ' + outdir + '/' + 'fastas')
